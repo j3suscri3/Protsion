068e7fd5910c597e5cd488c955e132e6fd273074
j3suscri3 <j3suscri3@gmail.com>
2020-05-14 01:10:39 +0200

    Delete version.hpp

--- source/common/version.hpp
+++
@@ -1,6 +0,0 @@
-#define DEF_VERSION
-#define DEF_ARGUMENT
-
-inline const std::string E_VERSION, E_ARCHITECTURE, E_HASH, H_NAME, H_CATEGORY, H_VERSION, H_CODE, C_NAME, C_VERSION, D_NAME, D_VERSION;
-
-#endif
